001b3ac937c25b6125aadcb31c4e7578a521c8a2
blachnierek <bla-max@wp.pl>
2018-01-24 09:25:11 +0100

    zmienne typy danych

--- TEB/js/3_typy_danych.js
+++ TEB/js/3_typy_danych.js
@@ -1,3 +1,4 @@
+/*
 var a = 10, b = 5;
 var wynik = a + b;
 
@@ -48,12 +49,41 @@ var liczba1 = prompt("Podaj Liczbę", "liczba1");
 var liczba2 = prompt("Podaj liczbę", "liczba2");
 var suma = liczba1 + liczba2;
 
-liczba1 = parseInt(liczba1);
-liczba1 = parseInt(liczba2);
+//liczba1 = parseInt(liczba1); //konwersja na typ całkowity
+//liczba1 = parseInt(liczba2);
+
+//konwersja na tym zmienno przecinkowy
+
+liczba1 = parsefloat (liczba1);
+liczba2 = parsefloat (liczba2);
+
 var suma1 = liczba1 + liczba2;
 console.log("Po konwersji typów: " + suma1);
 console.log(typeof(liczba1));
 console.log(typeof(liczba2));
+*/
+//########################################################################################################################
+//zad1
+
+/*
+Użytkownik podaje z klawiatury długośc boku kwadratu
+wyświetk na ekranie w formacie "Długość boku wynosi: .... cm "
+"Pole kwadratu wynosi: ....cm2"
+*/
+
+
+/*
+var dlugosc =prompt("Podaj długość boku:");
+var pole = dlugosc * dlugosc;
+document.write("Długość boku wynosi: " + dlugosc + "cm<br>");
+document.write("Pole kwadratu wynosi: " + pole + "cm<sup>2</sup>");
+*/
+
+
+
+
+//napisz program obliczający pole trójkąta
+
 
 
 
