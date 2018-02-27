a00cf98362289b477be1e9ae72dd8d7504f50d76
blachnierek <bla-max@wp.pl>
2018-02-27 16:42:22 +0100

    tablice 2

--- TEB/js/10_1_tablice.js
+++ TEB/js/10_1_tablice.js
@@ -12,3 +12,41 @@ function losowanie(){
 
 }
  losowanie();
+
+
+
+
+//uzytkownik podaje z klawiatury w polu numerycznym wartości ktore sa zapisywany do tablicy. Wyświetl wszystkie wartości z tablicy wyświetl ile jest ich łącznie i jaka jest ich suma oraz wartość z dokładnością do 2 miejsc po przecinku
+
+
+var liczba = document.getElementById("liczba");
+var przycisk = document.getElementById("przycisk");
+var wynik = document.getElementById("wynik");
+
+var liczby = [];
+
+function dodaj(){
+    let tab="";
+    let suma= 0;
+    liczby.push(liczba.value);
+
+    for(var i=0; i<liczby.length; i++){
+       tab += liczby[i] + " ";
+       suma += Nsumber(liczby[i]);
+    }
+    srednia = suma/liczby.length;
+    wynik.innerHTML = tab + "<br>" + "Ilośc elementów: " + liczby.length;
+    wynik.innerHTML = "Suma elementów: " + suma;
+
+
+}
+przycisk.addEventListener('click', dodaj);
+
+
+
+
+
+
+
+
+
