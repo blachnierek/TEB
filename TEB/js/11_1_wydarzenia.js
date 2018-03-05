effa20f4ec6b3d6e0091b0e0e97842e1f8ed734b
blachnierek <bla-max@wp.pl>
2018-03-05 23:59:23 +0100

    zadanie

--- TEB/js/11_1_wydarzenia.js
+++ TEB/js/11_1_wydarzenia.js
@@ -3,3 +3,15 @@ użytkownik podaje w polu tekstowym kod pocztowy
 pod polem wyświetl komunikat czy dane zostały prawidłowo wprowadzone
 Jeśli będą prawidłowe dane to wyświetl komunikat o treści "prawidłowy kod pocztowy: ....."
 jeśli będzie błędny to wyświetl komunikat "błędny kod pocztowy" i umieśc kursor w polu tekstowym*/
+
+
+var gozik = document.getElementById("gozik");
+var kod = document.getElementById("kod");
+var regula = (kod.value);
+regula = "/^\d{2}-\d{3}$/";
+var wynik = regula.test(regula);
+
+
+
+console.log(wynik);
+
