3b456696796139efc2543d892b789fbe35687b15
blachnierek <bla-max@wp.pl>
2018-03-14 10:20:43 +0100

    zajęcia

--- TEB/js/12_1_zdarzenia.js
+++ TEB/js/12_1_zdarzenia.js
@@ -1,8 +1,47 @@
 var elLitera = document.getElementById("litera");
 var elKod = document.getElementById("kod");
+var elX = document.getElementById("x")
+var elY = document.getElementById("y")
 
 function litera(){
-    console.log(event.keyCode);
+    let kod = event.keyCode;
+    switch(kod){
+        case 32:
+            kod = "space";
+            break;
+        case 8:
+            kod ="backspace";
+            break;
+        case 18:
+            kod = "alt";
+            break;
+        case 16:
+            kod = "shift";
+            break;
+        case 17:
+            kod = "ctrl";
+            break;
+        case 20:
+            kod = "CAPSLOCK"
+            break;
+
+
+
+
+    }
+    elKod.innerHTML = "Kod znaku: " + kod;
 }
 
+function pozycja() {
+    elX.value = event.screenX;
+    elY.value = event.screenY;
+}
+
+
+
+
+
+
+
 elLitera.addEventListener("keyup", litera);
+window.addEventListener("mousemove", pozycja);
---
+++ TEB/js/12_2_zdarzenia.js
@@ -0,0 +1,41 @@
+var elWys = document.getElementById("wysokosc");
+var elSzer = document.getElementById("szerokosc");
+var elBlok = document.getElementById("blok");
+var elKolor = document.getElementById("kolor");
+var elLewo = document.getElementById("lewo");
+var elDodajLewo = document.getElementById("DodajLewo");
+
+
+
+elBlok.style.backgroundColor = elKolor.value;
+elBlok.style.height = "0px";
+elBlok.style.width = "0px";
+
+function rozmiar(){
+        let sz = elSzer.value;
+        let w = elWys.value;
+        elBlok.style.height = sz + "px";
+        elBlok.style.width = w + "px";
+        console.log(sz);
+}
+
+function kolor(){
+    elBlok.style.backgroundColor = elKolor.value;
+}
+
+elLewo.value = elBlok.offsetLeft;
+
+function dl(){
+    elLewo.value = elBlok.offsetLeft + 10 +"px"
+}
+
+
+elSzer.addEventListener("change", rozmiar);
+elWys.addEventListener("change", rozmiar);
+elKolor.addEventListener("change", kolor);
+elDodajLewo.addEventListener("click", dl);
+
+
+
+
+
